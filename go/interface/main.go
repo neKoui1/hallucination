6692b6f2b4510e0bec6ca509dea51092b9d8f702
APTX4396 <77609544+neKoui1@users.noreply.github.com>
2025-12-23 15:54:29 +0800

    update

---
+++ go/interface/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+type interface2 struct {
+	Type unsafe.Pointer
+	Data unsafe.Pointer
+}
+
+func main() {
+	var x interface{}
+	x = 3
+	v2 := (*interface2)(unsafe.Pointer(&x))
+	fmt.Println(v2)
+	fmt.Println(unsafe.Pointer(&x))
+	fmt.Printf("%d\n", *(*int)(v2.Data))
+}
